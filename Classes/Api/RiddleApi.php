e94100bc89fc6b071b2b92104439cc907c7e0e65
Georg Ringer <georgringer@users.noreply.github.com>
2020-08-26 19:45:27 +0200

    Update RiddleApi.php

--- Classes/Api/RiddleApi.php
+++ Classes/Api/RiddleApi.php
@@ -11,9 +11,11 @@ namespace StudioMitte\Riddle\Api;
  * LICENSE.txt file that was distributed with this source code.
  */
 
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\HandlerStack;
 use StudioMitte\Riddle\ExtensionConfiguration;
-use TYPO3\CMS\Core\Http\Client\GuzzleClientFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class RiddleApi
@@ -51,6 +53,7 @@ class RiddleApi
 
     /**
      * @param string $action
+     * @param array $options
      * @param string $method
      * @return array
      * @throws GuzzleException
@@ -58,14 +61,14 @@ class RiddleApi
     protected function request(string $action, $method = 'GET'): array
     {
         $uri = self::ENDPOINT . trim($action, '/');
-        $client = GuzzleClientFactory::getClient();
+        $client = $this->getClient();
         $headers = [
             'Authorization' => 'Bearer ' . $this->extensionConfiguration->getApiToken(),
             'Key' => $this->extensionConfiguration->getApiKey(),
             'Accept' => 'application/json',
         ];
         $options['headers'] = $headers;
-        $response = $client->request($method, $uri);
+        $response = $client->request($method, $uri, $options);
 
         if ($response->getStatusCode() === 200) {
             return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
@@ -73,4 +76,23 @@ class RiddleApi
 
         throw new \UnexpectedValueException('Error for riddle request: ' . $response->getStatusCode(), 1597956586);
     }
+
+    /**
+     * Creates the client to do requests
+     * @return ClientInterface
+     */
+    private function getClient(): ClientInterface
+    {
+        $httpOptions = $GLOBALS['TYPO3_CONF_VARS']['HTTP'];
+        $httpOptions['verify'] = filter_var($httpOptions['verify'], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ?? $httpOptions['verify'];
+        if (isset($GLOBALS['TYPO3_CONF_VARS']['HTTP']['handler']) && is_array($GLOBALS['TYPO3_CONF_VARS']['HTTP']['handler'])) {
+            $stack = HandlerStack::create();
+            foreach ($GLOBALS['TYPO3_CONF_VARS']['HTTP']['handler'] ?? [] as $handler) {
+                $stack->push($handler);
+            }
+            $httpOptions['handler'] = $stack;
+        }
+        return GeneralUtility::makeInstance(Client::class, $httpOptions);
+    }
 }
+
